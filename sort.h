be849eb2087b0aeb30e2e0a55d918b6856f7ebd3
Zemichael <yetikimitabeba@gmail.com>
2023-07-25 00:52:10 +0300

    New prototype inserted for swap function.

--- sort.h
+++ sort.h
@@ -25,5 +25,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap(int *x, int *y);
 
 #endif /* SORT_H */
