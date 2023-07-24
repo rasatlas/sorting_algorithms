466401a9c0c971ec1d1e5addd9cbd4602d78652b
Zemichael <yetikimitabeba@gmail.com>
2023-07-24 23:58:28 +0300

    Insertion sort implementation on a doubly linked list.

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * insertion_sort - A function that sorts a doubly linked list of integers
+ * in ascending order using the insertion sort algorithm.
+ *
+ * @list: the doubly linked list to be sorted.
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	;
+}
