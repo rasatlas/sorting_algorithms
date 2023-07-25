0bd850b8c4629dbb1645fd8325d75221f4317c79
Zemichael <yetikimitabeba@gmail.com>
2023-07-25 18:19:37 +0300

    memory freeing exception included after each malloc.

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -26,6 +26,8 @@ void counting_sort(int *array, size_t size)
 	}
 
 	bucket_array = (int *)malloc((max_val + 1) * sizeof(max_val));
+	if (!bucket_array)
+		return;
 	for (i = 0; i < (size_t)(max_val + 1); i++)
 		bucket_array[i] = 0;
 
@@ -37,6 +39,11 @@ void counting_sort(int *array, size_t size)
 	print_array(bucket_array, (size_t)(max_val + 1));
 
 	sorted_array = (int *)(malloc(size));
+	if (!sorted_array)
+	{
+		free(bucket_array);
+		return;
+	}
 	for (i = 0; i < size; i++)
 		sorted_array[(bucket_array[array[i]]) - 1] = array[i];
 	for (i = 0; i < size; i++)
