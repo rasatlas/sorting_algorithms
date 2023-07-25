f6008b41df3759265a143b41e4127671b0e95676
Zemichael <yetikimitabeba@gmail.com>
2023-07-25 18:10:06 +0300

    Implementation of Counting sort on an array.

---
+++ 102-counting_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * counting_sort - A function that sorts an array of integers in ascending
+ * order using the Counting sort algorithm.
+ *
+ * @array: The array to be sorted.
+ * @size: size of the array.
+ */
+
+void counting_sort(int *array, size_t size)
+{
+	size_t i;
+	int *bucket_array, *sorted_array;
+	int max_val = array[0];
+
+	if (array == NULL || size == 1)
+		return;
+
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max_val)
+		{
+			max_val = array[i];
+		}
+	}
+
+	bucket_array = (int *)malloc((max_val + 1) * sizeof(max_val));
+	for (i = 0; i < (size_t)(max_val + 1); i++)
+		bucket_array[i] = 0;
+
+	for (i = 0; i < size; i++)
+		bucket_array[array[i]]++;
+
+	for (i = 1; i < (size_t)(max_val + 1); i++)
+		bucket_array[i] += bucket_array[i - 1];
+	print_array(bucket_array, (size_t)(max_val + 1));
+
+	sorted_array = (int *)(malloc(size));
+	for (i = 0; i < size; i++)
+		sorted_array[(bucket_array[array[i]]) - 1] = array[i];
+	array = sorted_array;
+	free(bucket_array);
+	free(sorted_array);
+}
