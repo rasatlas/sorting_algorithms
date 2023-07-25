afcb6da9942951da144e911db758491c109970e0
Zemichael <yetikimitabeba@gmail.com>
2023-07-25 18:37:00 +0300

    Making finding max value a function by itself.

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+int find_max(int*, size_t);
 
 /**
  * counting_sort - A function that sorts an array of integers in ascending
@@ -16,29 +17,17 @@ void counting_sort(int *array, size_t size)
 
 	if (array == NULL || size == 1)
 		return;
-
-	max_val = array[0];
-	for (i = 1; i < size; i++)
-	{
-		if (array[i] > max_val)
-		{
-			max_val = array[i];
-		}
-	}
-
+	max_val = find_max(array, size);
 	bucket_array = (int *)malloc((max_val + 1) * sizeof(max_val));
 	if (!bucket_array)
 		return;
 	for (i = 0; i < (size_t)(max_val + 1); i++)
 		bucket_array[i] = 0;
-
 	for (i = 0; i < size; i++)
 		bucket_array[array[i]]++;
-
 	for (i = 1; i < (size_t)(max_val + 1); i++)
 		bucket_array[i] += bucket_array[i - 1];
 	print_array(bucket_array, (size_t)(max_val + 1));
-
 	sorted_array = (int *)(malloc(size));
 	if (!sorted_array)
 	{
@@ -51,4 +40,24 @@ void counting_sort(int *array, size_t size)
 		array[i] = sorted_array[i];
 	free(bucket_array);
 	free(sorted_array);
+}
+
+/**
+ * find_max - finds the maximum value in an array.
+ * @array: The array to searched for max value.
+ * @size: size of the array.
+ * Return: returns the maximum value in the array.
+ */
+
+int find_max(int *array, size_t size)
+{
+	size_t i;
+	int max_val = array[0];
+
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max_val)
+			max_val = array[i];
+	}
+	return (max_val);
 }
